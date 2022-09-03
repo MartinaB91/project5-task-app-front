9d7c40a2293267cc236d32a884066b49215e610b
MartinaB91 <martinabjorlin@hotmail.com>
2022-09-03 11:53:08 +0000

    feat: Created and added basic styling for home page

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import { Container } from 'react-bootstrap';
 import { Routes, Route } from 'react-router-dom';
 import SignUpForm from "./pages/SignUp";
 import SignInForm from './pages/SignIn';
+import HomePage from './pages/Home';
 import "./api/axiosDefaults";
 
 const  App = () => {
   return (
     <div className="App">
       < NavBar />
-      <Container>
+      <Container fluid className="p-0">
         <Routes>
-          <Route exact path="/" element={<h1>Home</h1>} />
+          <Route exact path="/" element={<HomePage />} />
           <Route exact path="/signin" element={<SignInForm />} />
           <Route exact path="/signup" element={<SignUpForm />} />
           <Route exact path="/taskboard" element={<h1>Task board</h1>} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,25 @@
-
+import { Container } from "react-bootstrap";
+import HomeImage from "../assets/images/test-1.jpg"
+import styles from "../styles/Home.module.css"
 
 const HomePage = () => {
-    return(
-        <h1>Family Star</h1>
+    return (
+        <Container fluid id={styles["home-page-wrapper"]} className="p-0">
+            <div id={styles["home-text-wrapper"]}>
+                <h1 id={styles["home-header"]} className="text-start">Family Star</h1>
+                <p id={styles["home-info-text"]}>
+                    "Sed ut perspiciatis unde omnis iste natus error 
+                    sit voluptatem accusantium doloremque laudantium, 
+                    totam rem aperiam, eaque ipsa quae ab illo inventore 
+                    veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit 
+                    aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem 
+                    sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit 
+                    amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora. 
+                </p>
+            </div>
+            <img id={styles["home-img"]} src={HomeImage} alt="throphy" />
+        </Container>
     )
 }
 
