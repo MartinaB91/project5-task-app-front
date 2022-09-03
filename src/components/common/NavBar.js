dbe99bc702fabac7bd698f2c6f4c6d48d9e0edef
MartinaB91 <martinabjorlin@hotmail.com>
2022-09-03 11:55:51 +0000

    feat: Add styling to navbar

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { Navbar, Container, Nav} from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import styles from "../../styles/NavBar.module.css";
+
 
 const NavBar = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar className={styles.NavBar} bg="none" expand="lg" fixed="top">
             <Container fluid>
-                <Link to="/">
-                    <Navbar.Brand>Family Star <i className="fas fa-star"></i></Navbar.Brand>
+                <Link to="/" className={styles.Link}>
+                    <Navbar.Brand>Family Star<i className="fas fa-star" id={styles["star-icon"]}></i></Navbar.Brand>
                 </Link>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll" className="justify-content-end">
                     <Nav>
                         {/* <Navbar.Text>Change family member</Navbar.Text> */}
-                        <Link to="/signin">Sign In</Link>
+                        <Link to="/signin" className={styles.Link}>Sign In</Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
