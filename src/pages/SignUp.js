d7b70d09cbb68dcbfaaca98344f3ee371a894f8d
MartinaB91 <martinabjorlin@hotmail.com>
2022-09-07 15:18:50 +0000

    Added bootstrap styling to Sign up form

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom";
 import { Container, Alert } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 const SignUpForm = () => {
@@ -39,52 +41,57 @@ const SignUpForm = () => {
 
     return (
         <Container className={styles.Container}>
-            <Form onSubmit={handleFormSubmit}>
-                <Form.Group className="mb-3" controlId="username">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Username"
-                        name="username"
-                        onChange={onFormFieldUpdate}
-                        value={username}
-                    />
-                </Form.Group>
+            <h2>Sign Up</h2>
+            <Row>
+                <Col xs={12} md={10} lg={6} className="mx-auto text-start">
+                    <Form onSubmit={handleFormSubmit}>
+                        <Form.Group className="mb-3" controlId="username">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter username"
+                                name="username"
+                                onChange={onFormFieldUpdate}
+                                value={username}
+                            />
+                        </Form.Group>
 
-                <Form.Group className="mb-3" controlId="email">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control
-                        type="email"
-                        placeholder="Email"
-                        name="email"
-                        onChange={onFormFieldUpdate}
-                        value={email}
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        placeholder="Password"
-                        name="password1"
-                        onChange={onFormFieldUpdate}
-                        value={password1}
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="confirmPassword">
-                    <Form.Label>Confirm password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        placeholder="Confirm password"
-                        name="password2"
-                        onChange={onFormFieldUpdate}
-                        value={password2}
-                    />
-                </Form.Group>
-                <Button variant="secondary" type="submit">
-                    Sign Up
-                </Button>
-            </Form>
+                        <Form.Group className="mb-3" controlId="email">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                                name="email"
+                                onChange={onFormFieldUpdate}
+                                value={email}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Enter password"
+                                name="password1"
+                                onChange={onFormFieldUpdate}
+                                value={password1}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="confirmPassword">
+                            <Form.Label>Confirm password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Confirm password"
+                                name="password2"
+                                onChange={onFormFieldUpdate}
+                                value={password2}
+                            />
+                        </Form.Group>
+                        <Button variant="secondary" type="submit">
+                            Sign Up
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
         </Container>
     )
 }
