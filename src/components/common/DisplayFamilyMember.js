0f546de2a97bc06f9d39e16841b54bc98c62c3cc
MartinaB91 <martinabjorlin@hotmail.com>
2022-09-14 18:41:54 +0000

    feat: Add family members to nav dropdown menu

--- src/components/common/DisplayFamilyMember.js
+++ src/components/common/DisplayFamilyMember.js
@@ -1,9 +1,13 @@
 import { Image } from 'react-bootstrap';
 import axios from 'axios';
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "../../styles/DisplayFamilyMember.module.css"
+import Dropdown from 'react-bootstrap/esm/Dropdown';
+import { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons'
 
 export const DisplayFamilyMember = () => {
 
@@ -25,21 +29,24 @@ export const DisplayFamilyMember = () => {
       .catch((e) => console.log(e));
   };
 
+  // Only call once per "lifecycle", only run once. 
+  useEffect(() => {
+    handleFamilyMembers();
+
+  }, []);
+
   return (
     <>
-        <Row>
-          <Col>
-            {familymembers.map((familymember) => {
-              return (
-                <h1 key={familymember.name}>{familymember.name}</h1>
-                // <Image key={familymember.name} roundedCircle src={familymember.family_member_img}/>
-              )
-            })}
-          </Col>
-        </Row>
-        <Link to="/addfamilymember" className={styles.Link}>Add Family Member</Link>
-      <button onClick={handleFamilyMembers}>Show Family Members</button>
-
+      <Row>
+        <Col>
+          {familymembers.map((familymember) => {
+            return (
+              <Dropdown.Item href="#" key={familymember.name}>{familymember.name}</Dropdown.Item>
+            )
+          })}
+          <Link to="/addfamilymember" className={styles.Link}>Add Family Member<FontAwesomeIcon icon={faUserPlus} /></Link>
+        </Col>
+      </Row>
     </>
   )
 }
--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from 'axios';
-import { Navbar, Container, Nav, Alert, Row, Col } from "react-bootstrap";
+import { Navbar, NavDropdown, Container, Nav, Alert, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import styles from "../../styles/NavBar.module.css";
 import {
@@ -9,7 +9,9 @@ import {
 } from "../../context/CurrentUser";
 import SignInForm from "../../pages/SignIn";
 import { DisplayFamilyMember } from "../common/DisplayFamilyMember";
-
+import { Dropdown } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faStar } from '@fortawesome/free-solid-svg-icons'
 
 const NavBar = () => {
     const getCurrentUser = useCurrentUser();
@@ -31,8 +33,15 @@ const NavBar = () => {
         <>
             <Link to="/taskboard" className={styles.Link}>Task Board</Link>
             <Link to="/" className={styles.Link} onClick={handleSignOut}>Sign Out</Link>
-           
-            <DisplayFamilyMember />
+            <Dropdown>
+                <Dropdown.Toggle>
+                <FontAwesomeIcon icon={faUser} />
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <DisplayFamilyMember />
+                </Dropdown.Menu>
+            </Dropdown>
+
         </>
     );
 
@@ -43,16 +52,17 @@ const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} bg="none" expand="lg" fixed="top">
             <Container fluid>
-                    <Link to="/" className={styles.Link}>
-                        <Navbar.Brand>Family Star<i className="fas fa-star" id={styles["star-icon"]}></i></Navbar.Brand>
-                    </Link>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                    <Navbar.Collapse id="navbarScroll" className="justify-content-end">
-                        <Nav>
-                            {getCurrentUser ? buttonForSignedInUser : buttonForNotSignedInUser}
-                            {/* <Navbar.Text>Change family member</Navbar.Text> */}
-                        </Nav>
-                    </Navbar.Collapse>
+                <Link to="/" className={styles.Link}>
+                    <Navbar.Brand>Family Star
+                    <FontAwesomeIcon icon={faStar} id={styles["star-icon"]} /></Navbar.Brand>
+                </Link>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll" className="justify-content-end">
+                    <Nav>
+                        {getCurrentUser ? buttonForSignedInUser : buttonForNotSignedInUser}
+                        {/* <Navbar.Text>Change family member</Navbar.Text> */}
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
 
