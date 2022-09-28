9ed712b59ea1f8360e06dc3ca8c7d4ec1312babd
MartinaB91 <martinabjorlin@hotmail.com>
2022-09-28 07:31:32 +0000

    feat: Add functionality for view profile on taskboard page

---
+++ src/components/family_member/FamilyMemberScoreBoard.js
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect } from 'react';
+import { useCurrentUser, setCurrentUser, CurrentUserContext } from "../../context/CurrentUser";
+import { useParams } from "react-router-dom";
+import { useState } from 'react';
+import axios from 'axios';
+import { Container } from 'react-bootstrap';
+
+export const FamilyMemberScoreBoard = () => {
+    const currentUser = useCurrentUser();
+    const { id } = useParams();
+
+    const [profile, setProfile] = useState({});
+
+    useEffect(() => {
+        const handleMount = async () => {
+            await axios.get(`profiles/${currentUser["profile_id"]}`)
+                .then((response) => {
+                    // Set profile to Data
+                    setProfile(response["data"]);
+                })
+                .catch((e) => console.log(e));
+        };
+        handleMount();
+    }, [id]);
+
+    return (
+        <Container>
+            <h4>{currentUser.username} Scoreboard</h4>
+            <h4>{profile["user"]} user</h4>
+            <h4>{profile["created_at"]} created_at</h4>
+            <h4>{profile["updated_at"]} updated_at</h4>
+        </Container>
+    )
+}
