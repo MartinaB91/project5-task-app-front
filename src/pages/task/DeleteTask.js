7f9c4c4cf87a2f73f76633428acad15d32a5e5e5
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-05 08:53:59 +0000

    feat: Style background, headers and added media queries to DeleteTask form

--- src/pages/task/DeleteTask.js
+++ src/pages/task/DeleteTask.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { axiosReq } from '../../api/axiosDefaults';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Col, Row } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import { Image } from "react-bootstrap";
+import Rabbit from "../../assets/images/rabbit-3.jpg";
+import styles from "../../styles/DeleteTask.module.css";
 
 export const DeleteTask = () => {
     const { id, title } = useParams();
@@ -20,10 +23,15 @@ export const DeleteTask = () => {
         }
     };
 
-  return (
-    <Container>
-        <p className='mt-5'>Want to delete task {title}?</p>
-        <Button variant="dark"  onClick={handleDelete}>Delete</Button>
-    </Container>
-  )
+    return (
+        <Container fluid className={styles.Container}>
+            <Row>
+                <Col xs={12} sm={10} md={6} lg={4} className={`${styles.FormWrapper} mx-auto`}>
+                    <h4 className={styles.DeleteHeader}>Want to delete task {title}?</h4>
+                    <Button className={`${styles.DeleteButton} mt-2`} variant="dark" onClick={handleDelete}>Delete</Button>
+                </Col>
+            </Row>
+            <Image className={styles.BackgroundImage} src={Rabbit} />
+        </Container>
+    )
 }
