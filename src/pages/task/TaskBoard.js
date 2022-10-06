a2be5697bd8f0d454beff9a4df6d58b784c87cb0
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-06 06:48:44 +0000

    refactor: Moved and added functionality to search tasks

--- src/pages/task/TaskBoard.js
+++ src/pages/task/TaskBoard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { MyScoreBoard } from "../../components/family_member/MyScoreBoard";
-import { DisplayFamilyMemberTasks } from "../task/FamilyMemberTasks";
+import { DisplayFamilyMemberTasks } from "../task/Tasks";
 import { Form } from "react-bootstrap";
 import styles from "../../styles/TaskBoard.module.css";
 import { ProfileScoreBoard } from "../../components/family_member/ProfileScoreBoard";
@@ -10,24 +10,6 @@ import { axiosReq } from "../../api/axiosDefaults";
 
 
 const TaskBoard = () => {
-
-    const [query, setQuery] = useState();
-
-    useEffect(() => {
-        const fetchPosts = async () => {
-          try {
-            const { data } = await axiosReq.get(`taskboard/tasks/?search=${query}`);
-          } catch (err) {
-            console.log(err);
-          }
-        };
-        const timer = setTimeout(() => {
-            
-        }, 1000)
-        fetchPosts();
-    
-      }, [query]);
-
     return (
         <Container className={styles.Container}>
             <Row>
@@ -37,7 +19,7 @@ const TaskBoard = () => {
             </Row>
             <Row>
             <Col className="flex-start" id={styles["todo-tasks-wrapper"]} xs={{ span: 12, order: 2 }} md={{ span: 10, order: 1 }}>
-                    <Form className={`${styles.SearchForm} mb-4`} onSubmit={(event) => event.preventDefault()}>
+                    {/* <Form className={`${styles.SearchForm} mt-3 mb-4`} onSubmit={(event) => event.preventDefault()}>
                         <Form.Control 
                         value={query}
                         onChange={(event) => setQuery(event.target.value)}
@@ -45,7 +27,7 @@ const TaskBoard = () => {
                         placeholder="Search for tasks"
                         ></Form.Control>
 
-                    </Form>
+                    </Form> */}
                 <DisplayFamilyMemberTasks />
                 </Col>
                 <Col className="flex-start" id={styles["my-score-board"]} xs={{ span: 12, order: 1 }} md={{ span: 2, order: 2 }}>
--- src/pages/task/Tasks.js
+++ src/pages/task/Tasks.js
@@ -9,6 +9,8 @@ import { CurrentFamilyMemberContext } from "../../context/CurrentFamilyMemberCon
 import { EllipsisDropdown } from "../../components/task/TaskEllipsisButtons";
 import Image from "react-bootstrap/Image";
 import Test from "../../assets/images/test-sign-in.jpg";
+import { Form } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefaults";
 
 
 export const DisplayFamilyMemberTasks = () => {
@@ -21,6 +23,25 @@ export const DisplayFamilyMemberTasks = () => {
   const [familymembersList, setFamilymembersList] = useState([]);
   const [error, setError] = useState({});
   const [familymemberName, setFamilymemberName] = useState([]);
+  const [query, setQuery] = useState();
+
+  useEffect(() => {
+    const handleFamilyMembersList = async () => {
+      await axios.get("familymembers/members/")
+        .then((response) => {
+          console.log(response);
+
+          // "Convert" json to array
+          let responseAsArray = [];
+          for (let resp of response.data) {
+            responseAsArray.push(resp);
+          }
+          setFamilymembersList(responseAsArray);
+        })
+        .catch((e) => console.log(e));
+    };
+    handleFamilyMembersList();
+  }, []);
 
   useEffect(() => {
     const handleMount = async () => {
@@ -39,24 +60,29 @@ export const DisplayFamilyMemberTasks = () => {
         .catch((e) => console.log(e));
     };
 
-    const handleFamilyMembersList = async () => {
-      await axios.get("familymembers/members/")
-        .then((response) => {
-          console.log(response);
+    const fetchTasks = async () => {
+      if (query !== null && query !== "" && query !== undefined) {
+        await axios.get(`taskboard/tasks/?search=${query}`)
+          .then((response) => {
+            console.log(response);
 
-          // "Convert" json to array
-          let responseAsArray = [];
-          for (let resp of response.data) {
-            responseAsArray.push(resp);
-          }
-          setFamilymembersList(responseAsArray);
-        })
-        .catch((e) => console.log(e));
-    };
+            // "Convert" json to array
+            let tasksAsArray = [];
+            for (let resp of response.data) {
+              tasksAsArray.push(resp);
+            }
+            setTasks(tasksAsArray);
+          })
+          .catch((e) => console.log(e));
+      };
+    }
+
+    const timer = setTimeout(() => {
+      fetchTasks();
+    }, 1000)
 
     handleMount();
-    handleFamilyMembersList();
-  }, []);
+  }, [query]);
 
   const handleAssign = async (e) => {
     e.preventDefault();
@@ -72,7 +98,7 @@ export const DisplayFamilyMemberTasks = () => {
             if (task.assigned === currentFamilyMemberObj.id) {
               task.assigned = null;
               currentFamilyMemberObj.ongoing_tasks = currentFamilyMemberObj.ongoing_tasks - 1;
-              
+
             } else {
               task.assigned = currentFamilyMemberObj.id;
               currentFamilyMemberObj.ongoing_tasks = currentFamilyMemberObj.ongoing_tasks + 1;
@@ -82,9 +108,6 @@ export const DisplayFamilyMemberTasks = () => {
         }
         setTasks(tasksAsArray);
         setFamilyMemberContext(JSON.stringify(currentFamilyMemberObj));
-
-        
-
       })
       .catch((e) => console.log(e));
   };
@@ -92,8 +115,8 @@ export const DisplayFamilyMemberTasks = () => {
   const handleTaskDone = async (e) => {
     e.preventDefault();
 
-      const taskId = e.currentTarget.value;
-      axios.patch(`taskboard/tasks/${taskId}/done`, { "status": "Done" }) 
+    const taskId = e.currentTarget.value;
+    axios.patch(`taskboard/tasks/${taskId}/done`, { "status": "Done" })
       .then((response) => {
         const tasksAsArray = [];
         for (let task of tasks) {
@@ -105,70 +128,90 @@ export const DisplayFamilyMemberTasks = () => {
         }
         setTasks(tasksAsArray);
 
-        if (response["data"].status == "Todo"){
+        if (response["data"].status == "Todo") {
           currentFamilyMemberObj.star_points = currentFamilyMemberObj.star_points - response["data"].star_points;
           currentFamilyMemberObj.closed_tasks = currentFamilyMemberObj.closed_tasks - 1;
-        } else{
+        } else {
           currentFamilyMemberObj.star_points = currentFamilyMemberObj.star_points + response["data"].star_points;
           currentFamilyMemberObj.closed_tasks = currentFamilyMemberObj.closed_tasks + 1;
         }
-  
+
         setFamilyMemberContext(JSON.stringify(currentFamilyMemberObj));
-        
+
       })
       .catch((e) => console.log(e));
-    };
+  };
 
   return (
-    <Row className="g-2">
-      {tasks.map((task) => {
-        return (
-          <Col key={task.id} sm={12} md={6} lg={4}>
-            <Card className="shadow-sm">
-              <Card.Header className={styles.CardTitle}>
-                <Card.Title className="text-start" >
+    <>
+      <Row>
+        <Form className={`${styles.SearchForm} mt-3 mb-4`} onSubmit={(event) => event.preventDefault()}>
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            placeholder="Search for tasks"
+          ></Form.Control>
+
+        </Form>
+      </Row>
+      <Row className="g-2">
+        {tasks.map((task) => {
+          return (
+            <Col key={task.id} sm={12} md={6} lg={4}>
+              <Card className="shadow-sm">
+                <Card.Header className={styles.CardTitle}>
+                  <Card.Title className="text-start" >
+                    <Row>
+                      <Col xs={1} sm={1} md={2} lg={1}>< EllipsisDropdown title={task.title} id={task.id} /></Col>
+                      <Col xs={8} sm={8} md={6} lg={7} className={`${styles.Button} ${styles.taskTitle} text-center`}>{task.title}</Col>
+                      <Col xs={3} sm={3} md={4} lg={4} className={`${styles.Button} text-end`}>{task.star_points}<FontAwesomeIcon className={`${styles.FontAwesomeIcon}`} icon={faStar} /></Col>
+                    </Row>
+                  </Card.Title>
+                </Card.Header>
+                <Card.Body className={styles.CardBody}>
+                  <Card.Text className={`${styles.endDate} text-end`}>
+                    Done: {task.end_date}
+                  </Card.Text>
+                  <Card.Text className="text-center">
+                    {task.description}
+                  </Card.Text>
                   <Row>
-                    <Col xs={1} sm={1} md={2} lg={1}>< EllipsisDropdown title={task.title} id={task.id} /></Col>
-                    <Col xs={8} sm={8} md={6} lg={7} className={`${styles.Button} ${styles.taskTitle} text-center`}>{task.title}</Col>
-                    <Col xs={3} sm={3} md={4} lg={4} className={`${styles.Button} text-end`}>{task.star_points}<FontAwesomeIcon className={`${styles.FontAwesomeIcon}`}icon={faStar} /></Col>
-                  </Row>
-                </Card.Title>
-              </Card.Header>
-              <Card.Body className={styles.CardBody}>
-                <Card.Text className={`${styles.endDate} text-end`}>
-                  Done: {task.end_date}
-                </Card.Text>
-                <Card.Text>
-                  {task.description}
-                </Card.Text>
-                <Row>
-                  <Col xs={2} md={2}>
-                    {task.assigned === null || task.assigned === "" ?
-                      <Button onClick={handleAssign} value={task.id} className="text-start" variant="link"><FontAwesomeIcon icon={faUserPlus} className={`${styles.userPlus} fa-2x btn`} /></Button>
-                      :
-                      <>
-                      <button onClick={handleAssign} value={task.id} className={styles.AssignButton}><Image roundedCircle src={Test} className={styles.Image} /></button>
-                      <p>{}</p>
-                      </>
+                    <Col xs={2} md={2}>
+                      {task.assigned === null || task.assigned === "" ?
+                        <Button onClick={handleAssign} value={task.id} className="text-start" variant="link"><FontAwesomeIcon icon={faUserPlus} className={`${styles.userPlus} fa-2x btn`} /></Button>
+                        :
+                        <>
+                          <button onClick={handleAssign} value={task.id} className={styles.AssignButton}><Image roundedCircle src={Test} className={styles.Image} /></button>
+                          <p>{ }</p>
+                        </>
                       }
 
-                  </Col>
-                  <Col xs={7} md={7}>
-                    <Card.Text className="text-center mt-3">{task.category_name}</Card.Text>
-                  </Col>
-                  <Col xs={3} md={3}>
-                    {task.status === "Todo" ?
-                      <Button variant="link" onClick={handleTaskDone} value={task.id} className="text-end btn"><FontAwesomeIcon icon={faCircleCheck} size="lg" className={`${styles.checkMark} fa-2x btn`} /></Button>
-                      : <Button variant="link" onClick={handleTaskDone} value={task.id} className="text-end"><FontAwesomeIcon icon={faCircleCheck} size="lg" className={`${styles.checkMarkDone} fa-2x text-end btn`} /></Button>}
+                    </Col>
+                    <Col xs={7} md={7}>
+                      <Card.Text className="text-center mt-3">{task.category_name}</Card.Text>
+                    </Col>
+                    <Col xs={3} md={3}>
+                      {/* If task status is todo but the task is not assigned the done btn will be
+                      disabled. When btn is assigned you can mark the task as done */}
+                      {task.status === "Todo" ?
+                        <>
+                          {task.assigned === null || task.assigned === "" ?
+                            <Button disabled variant="link" onClick={handleTaskDone} value={task.id} className="text-end btn"><FontAwesomeIcon icon={faCircleCheck} size="lg" className={`${styles.checkMark} fa-2x btn`} /></Button>
+                            :
+                            <Button variant="link" onClick={handleTaskDone} value={task.id} className="text-end btn"><FontAwesomeIcon icon={faCircleCheck} size="lg" className={`${styles.checkMark} fa-2x btn`} /></Button>}
+                        </>
+                        : <Button variant="link" onClick={handleTaskDone} value={task.id} className="text-end"><FontAwesomeIcon icon={faCircleCheck} size="lg" className={`${styles.checkMarkDone} fa-2x text-end btn`} /></Button>}
 
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-          </Col>
-        )
-      })}
-    </Row>
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </Col>
+          )
+        })}
+      </Row>
+    </>
   )
 }
 
