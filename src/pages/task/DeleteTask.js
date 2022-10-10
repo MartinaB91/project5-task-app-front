f201684546deb8d09af912ed04ce374d68e086c3
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-10 13:49:55 +0000

    feat: Add child limitation for delete task, DeleteTask

--- src/pages/task/DeleteTask.js
+++ src/pages/task/DeleteTask.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { axiosReq } from '../../api/axiosDefaults';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import { Alert } from 'react-bootstrap';
 import { Image } from "react-bootstrap";
+import { CurrentFamilyMemberContext } from "../../context/CurrentFamilyMemberContext";
 import Rabbit from "../../assets/images/rabbit-3.webp";
 import styles from "../../styles/DeleteTask.module.css";
 
@@ -12,6 +14,8 @@ export const DeleteTask = () => {
     const { id, title } = useParams();
     const navigate = useNavigate();
     const [error, setError] = useState({});
+    const [familyMemberContext] = useContext(CurrentFamilyMemberContext);
+    const currentFamilyMemberObj = JSON.parse(familyMemberContext);
 
     const handleDelete = async (e) => {
         try {
@@ -28,7 +32,11 @@ export const DeleteTask = () => {
             <Row>
                 <Col xs={12} sm={10} md={6} lg={4} className={`${styles.FormWrapper} mx-auto`}>
                     <h4 className={styles.DeleteHeader}>Want to delete task {title}?</h4>
-                    <Button className={`${styles.DeleteButton} mt-2`} variant="dark" onClick={handleDelete}>Delete</Button>
+                    {currentFamilyMemberObj.role == 1 ?
+                        <Button className={`${styles.DeleteButton} mt-2`} variant="dark" onClick={handleDelete}>Delete</Button>
+                        :
+                        <Alert variant='warning' className='text-center'>Ask your parents to delete task</Alert>
+                    }
                 </Col>
             </Row>
             <Image className={styles.BackgroundImage} src={Rabbit} />
