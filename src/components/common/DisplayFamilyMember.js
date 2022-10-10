4cd0d056ce6fb9bada04044b1f5237ce11db7ba4
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-10 12:03:34 +0000

    feat: Add text if profile has no family members, DisplayFamilyMembers

--- src/components/common/DisplayFamilyMember.js
+++ src/components/common/DisplayFamilyMember.js
@@ -7,7 +7,7 @@ import styles from "../../styles/DisplayFamilyMember.module.css"
 import Dropdown from 'react-bootstrap/esm/Dropdown';
 import { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { faUserPlus, faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { CurrentFamilyMemberContext } from "../../context/CurrentFamilyMemberContext";
 
 export const DisplayFamilyMember = () => {
@@ -37,20 +37,27 @@ export const DisplayFamilyMember = () => {
 
   return (
     <>
-      <Row>
-        <h4 className='mb-4 mt-2'>Choose a family member</h4>
+      <h4 className='mb-4 mt-2 text-center'>Choose a family member</h4>
+      {familymembers == "" ?
+        <>
+          <p className='text-center mt-2'>You haven’t added any family members yet.</p>
+          <p className='text-center mb-5'>Create one below <FontAwesomeIcon icon={faArrowDown} /></p>
+        </>
+        :
+        <Row>
           {familymembers.map((familymember) => {
             return (
-              <Col xs={6} md={4}  key={familymember.id}>
-                <Link  className={styles.Link} onClick={() => setFamilyMemberContext(JSON.stringify(familymember))} to="taskboard/">
+              <Col xs={6} md={4} key={familymember.id}>
+                <Link className={styles.Link} onClick={() => setFamilyMemberContext(JSON.stringify(familymember))} to="taskboard/">
                   <Image roundedCircle className={styles.Image} src={familymember.family_member_img}></Image>
                   <p className={styles.MemberDropdownText}>{familymember.name}</p>
                 </Link>
               </Col>
-              )
+            )
           })}
-          <Link to="/addfamilymember" className={styles.Link}>Add Family Member<FontAwesomeIcon icon={faUserPlus} /></Link>
-      </Row>
+        </Row>
+      }
+      <Link to="/addfamilymember" className={styles.Link}>Add Family Member<FontAwesomeIcon icon={faUserPlus} /></Link>
     </>
   )
 }
