fc77732511a9e7c65df060b96cc5084f19667e30
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-12 19:25:37 +0000

    feat: Made taskboard only viewable for authenticated users

--- src/pages/task/TaskBoard.js
+++ src/pages/task/TaskBoard.js
@@ -1,29 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { MyScoreBoard } from "../family_member/MyScoreBoard";
 import { DisplayFamilyMemberTasks } from "../task/Tasks";
-import { Form } from "react-bootstrap";
 import styles from "../../styles/TaskBoard.module.css";
 import { ProfileScoreBoard } from "../family_member/ProfileScoreBoard";
-import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser, setCurrentUser } from "../../context/CurrentUser";
+import { Link } from "react-router-dom";
 
 const TaskBoard = () => {
+    const currentUser = useCurrentUser();
     return (
-        <Container className={styles.Container}>
-            <Row>
-                <Col lg={12} id={styles["family-member-score-board"]} className="mb-2 mb-md-3">
-                    <ProfileScoreBoard />
-                </Col>
-            </Row>
-            <Row>
-            <Col className="flex-start" id={styles["todo-tasks-wrapper"]} xs={{ span: 12, order: 2 }} md={{ span: 10, order: 1 }}>
-                <DisplayFamilyMemberTasks />
-                </Col>
-                <Col className="flex-start" id={styles["my-score-board"]} xs={{ span: 12, order: 1 }} md={{ span: 2, order: 2 }}>
-                    <MyScoreBoard />
-                </Col>
-            </Row>
-        </Container>
+        <>
+            {currentUser == null || currentUser === "" ? (
+                <Container fluid className={styles.NotAutenticatedUserContainer}>
+                    <p>You need to sign in before viewing the taskboard</p>
+                    <Link to="/signin">Sign In</Link>
+                </Container>
+            ) : (
+                <Container className={styles.Container}>
+                    <Row>
+                        <Col lg={12} id={styles["family-member-score-board"]} className="mb-2 mb-md-3">
+                            <ProfileScoreBoard />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="flex-start" id={styles["todo-tasks-wrapper"]} xs={{ span: 12, order: 2 }} md={{ span: 10, order: 1 }}>
+                            <DisplayFamilyMemberTasks />
+                        </Col>
+                        <Col className="flex-start" id={styles["my-score-board"]} xs={{ span: 12, order: 1 }} md={{ span: 2, order: 2 }}>
+                            <MyScoreBoard />
+                        </Col>
+                    </Row>
+                </Container>
+
+            )}
+        </>
+
     )
 };
 
