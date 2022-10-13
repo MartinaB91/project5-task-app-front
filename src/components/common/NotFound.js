f7a7e392571f23e8df1d689fc389647c4c9e5f88
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-13 19:28:59 +0000

    style: Remove space from NotFound

--- src/components/common/NotFound.js
+++ src/components/common/NotFound.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
-import HiddenRabbit from "../../assets/images/404-page.svg";
+import HiddenRabbit from "../../assets/images/hidden-rabbit.svg";
 import styles from "../../styles/NotFound.module.css";
 
 
@@ -12,10 +12,6 @@ const NotFound = () => {
         <Image roundedCircle src={HiddenRabbit} className={styles.Image}/>
         <h4 className={styles.NotFoundText}>Oops.... This page don't exist.</h4>
         <p className='text-center'>404 ERROR</p>
-       
-
-
-
     </Container>
   )
 }
