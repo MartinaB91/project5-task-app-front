34b31e1d572621038320cdb7f6ebf5d54dd64730
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-13 18:45:22 +0000

    refactor: Moved message for unautenticated users to own component

---
+++ src/components/common/MessageUnauthenticatedUser.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import RabbitFace from "../../assets/images/rabbit-face-1.svg";
+import styles from "../../styles/MessageUnauthenticatedUser.module.css";
+import Image from "react-bootstrap/Image";
+import { Container, Row } from "react-bootstrap";
+
+const MessageUnauthenticatedUser = ({ message }) => {
+    return (
+        <Container className={styles.Container}>
+            <Row className="d-md-block justify-content-center">
+                <Image src={RabbitFace} className={`${styles.RabbitFace} mt-3`} />
+                <p className={"text-center"}>{message}</p>
+            </Row>
+        </Container>
+    )
+}
+
+export default MessageUnauthenticatedUser
+
+
+
