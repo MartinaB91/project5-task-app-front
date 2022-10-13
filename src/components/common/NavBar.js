1ce3f8cd911249638739847041da6bee5f7b697b
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-13 19:28:29 +0000

    refactor: Remove link from Dropdown.Item, Navbar

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -35,20 +35,18 @@ const NavBar = () => {
 
     const buttonForSignedInUser = (
         <>
-        
-            <Link to="/taskboard" className={`${styles.Link} pt-2`}>Task Board</Link>
-            <Dropdown align="end">
+
+            <Dropdown align="end" className={styles.Test}>
                 <Dropdown.Toggle variant="link" className={styles.Link}>
                     {getCurrentUser?.username}
                 </Dropdown.Toggle>
                 <Dropdown.Menu className={`${styles.Menu} p-3 mt-2`} >
                     <Dropdown.Item><DisplayFamilyMember /></Dropdown.Item>
-                    <Dropdown.Item>
-                        <Link to="/addtask" className={styles.Link}>Add Task <FontAwesomeIcon icon={faCirclePlus} /></Link>
+                    <Dropdown.Item href="/addtask" className={styles.Link}>
+                        Add Task <FontAwesomeIcon icon={faCirclePlus} className={styles.AddTaskIcon} />
                     </Dropdown.Item>
-                    <Dropdown.Item>
-                        <Link to="/" className={styles.Link} onClick={handleSignOut}>Sign Out</Link>
-                    </Dropdown.Item>
+                    <Dropdown.Item href="/taskboard" className={`${styles.Link} pt-2`}>Task Board</Dropdown.Item>
+                    <Dropdown.Item href="/" className={styles.Link} onClick={handleSignOut}>Sign Out</Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
 
@@ -63,7 +61,7 @@ const NavBar = () => {
         <Navbar className={styles.NavBar} fixed="top">
             <Container fluid className={styles.Container}>
                 <Link to="/" className={styles.Link}>
-                    <Navbar.Brand className={`${styles.Headers} p-3`}>Family Star 
+                    <Navbar.Brand className={`${styles.Headers} p-3`}>Family Star
                         <FontAwesomeIcon icon={faStar} id={styles["star-icon"]} /></Navbar.Brand>
                 </Link>
                 <Navbar.Toggle aria-controls="navbarScroll" />
