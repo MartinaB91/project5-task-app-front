b6dcf5bb1e3efe1822532f0c0bc1d29dddf2c41d
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-16 09:53:49 +0000

    chore: Change to deploy URL

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 // Deploy URL
-// axios.defaults.baseURL = 'https://project5-task-app-back.herokuapp.com/'
+axios.defaults.baseURL = 'https://project5-task-app-back.herokuapp.com/'
 // Local URL
-axios.defaults.baseURL = 'https://8000-martinab91-project5task-hc5012iima2.ws-eu70.gitpod.io/'
+// axios.defaults.baseURL = 'https://8000-martinab91-project5task-hc5012iima2.ws-eu71.gitpod.io/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
 
