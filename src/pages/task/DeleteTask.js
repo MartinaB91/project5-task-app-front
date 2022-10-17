3c4638547b2137246d9d5e2cbafa7eae31a6581e
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-17 18:56:56 +0000

    feat: Add error handling for edit task

--- src/pages/task/DeleteTask.js
+++ src/pages/task/DeleteTask.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { axiosReq } from '../../api/axiosDefaults';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import { Alert } from 'react-bootstrap';
@@ -11,7 +11,7 @@ import Rabbit from "../../assets/images/rabbit-3.webp";
 import styles from "../../styles/DeleteTask.module.css";
 
 export const DeleteTask = () => {
-    const { id, title } = useParams();
+    const { id, title, assigned } = useParams();
     const navigate = useNavigate();
     const [error, setError] = useState({});
     const [familyMemberContext] = useContext(CurrentFamilyMemberContext);
@@ -32,10 +32,23 @@ export const DeleteTask = () => {
             <Row>
                 <Col xs={12} sm={10} md={6} lg={4} className={`${styles.FormWrapper} mx-auto`}>
                     <h4 className={styles.DeleteHeader}>Want to delete task {title}?</h4>
-                    {currentFamilyMemberObj.role === 1 ?
-                        <Button className={`${styles.DeleteButton} mt-2`} variant="dark" onClick={handleDelete}>Delete</Button>
-                        :
-                        <Alert variant='warning' className='text-center mt-5'>Ask your parents to delete task</Alert>
+                    {currentFamilyMemberObj?.id !== null ? (
+                        <>
+                            {currentFamilyMemberObj.role === 1 && assigned ==="null" ?
+                                <Button className={`${styles.DeleteButton} mt-2`} variant="dark" onClick={handleDelete}>Delete</Button>
+                                :
+                                <>
+                                    {currentFamilyMemberObj.role === 0 ?
+                                        <Alert variant='warning' className='text-center mt-5'>Ask your parents to delete task</Alert>
+                                        :
+                                        <Alert variant='warning' className='text-center mt-5'>You can't delete an assigned task</Alert>
+                                    }
+                                </>
+                            }
+                        </>
+                    ) : (
+                        <Alert variant='warning' className='text-center mt-5'>You need to choose a family member first</Alert>
+                    )
                     }
                 </Col>
             </Row>
