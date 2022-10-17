fc65995edc6cc6b5f4f9eaeb14a8916d10850420
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-17 08:16:27 +0000

    feat: Add if statement to try remove refresh

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -7,6 +7,9 @@ export const setTokenTimestamp = (data) => {
 };
 
 export const shouldRefreshToken = () => {
+  if (localStorage.getItem("refreshTokenTimestamp") == null) {
+    return false;
+  }
   return !!localStorage.getItem("refreshTokenTimestamp");
 };
 
