9122ca063cd10b033c64e6cce11b569e0aeaa4b0
MartinaB91 <martinabjorlin@hotmail.com>
2022-10-19 10:06:50 +0000

    refactor: Remove unused import, DeleteTask

--- src/pages/task/DeleteTask.js
+++ src/pages/task/DeleteTask.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { axiosReq } from '../../api/axiosDefaults';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useState, useContext, useEffect } from 'react';
+import { useState, useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
